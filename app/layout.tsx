48f5c727c76b510498822d070e2a513f064a87e6
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2026-07-03 23:10:09 +0300

    coment all unused

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Montserrat } from 'next/font/google';
 import TanStackProvider from '@/components/TanStackProvider';
-import AuthProvider from '@/components/AuthProvider';
+// import AuthProvider from '@/components/AuthProvider';
 // import Header from '@/components/Header';
 // import Footer from '@/components/Footer';
 
