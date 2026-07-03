fd76f28ef0f49e74e008370d071b97e297c57c72
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2026-07-03 23:07:09 +0300

    add root layout & no any & some aditional eslint TS rules

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	reactCompiler: true,
-	images: {
-		remotePatterns: [{ protocol: 'https', hostname: 'res.cloudinary.com' }],
-	},
-	turbopack: {
-		root: __dirname,
-	},
-}
+  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: 'ftp.goit.study' },
+    ],
+  },
+  turbopack: {
+    root: __dirname,
+  },
+};
 
-export default nextConfig
-
+export default nextConfig;
