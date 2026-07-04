e8d6847be94371ad79bd301f245fa3728cb20626
Milena Karpenko <milenness06@gmail.com>
2026-07-04 11:35:38 +0300

    Upd trevallerCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
 // import css from './page.module.css';
-// import { TravellerCard } from '../components/TravellerCard/TravellerCard';
 
 export default function Home() {
   return (
     <div className="container">
       Home page
-      {/* <TravellerCard
-        name="Анастасія Олійник"
-        articlesCount={12}
-        photoUrl="/path-to-your-image.jpg"
-      /> */}
     </div>
   );
 }
