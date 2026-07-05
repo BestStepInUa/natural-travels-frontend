31e6126d64f9619009e0457c76da0aa7e90c7b8d
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2026-07-05 18:26:38 +0300

    fix tab

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import {
@@ -13,62 +12,64 @@ export default function Footer() {
   const year = new Date().getFullYear();
 
   return (
-    <footer className={css.footer}>
-      <Link href="/" className={css.logo} aria-label="Перейти на головну">
-        <Image
-          src="/icons/logo.svg"
-          alt="Природні Мандри"
-          width={24}
-          height={24}
-        />
-        <span>
-          Природні
-          <br />
-          Мандри
-        </span>
-      </Link>
-      <ul className={css.socialList} aria-label="Соціальні мережі">
-        <li>
-          <a
-            href="https://facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Facebook"
-          >
-            <FaFacebook className={css.socialIcon} />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://instagram.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Instagram"
-          >
-            <FaInstagram className={css.socialIcon} />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://twitter.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="X(раніше Twitter)"
-          >
-            <FaXTwitter className={css.socialIcon} />
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://youtube.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="YouTube"
-          >
-            <FaYoutube className={css.socialIcon} />
-          </a>
-        </li>
-      </ul>
+    <footer className={`${css.footer} container`}>
+      <div className={css.logo_and_social_wrapper}>
+        <Link href="/" className={css.logo} aria-label="Перейти на головну">
+          <Image
+            src="/icons/logo.svg"
+            alt="Природні Мандри"
+            width={24}
+            height={24}
+          />
+          <span>
+            Природні
+            <br />
+            Мандри
+          </span>
+        </Link>
+        <ul className={css.socialList} aria-label="Соціальні мережі">
+          <li>
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Facebook"
+            >
+              <FaFacebook className={css.socialIcon} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Instagram"
+            >
+              <FaInstagram className={css.socialIcon} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="X(раніше Twitter)"
+            >
+              <FaXTwitter className={css.socialIcon} />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="YouTube"
+            >
+              <FaYoutube className={css.socialIcon} />
+            </a>
+          </li>
+        </ul>
+      </div>
       <nav className={css.nav} aria-label="Навігація">
         <Link href="/" aria-label="Головна">
           Головна
