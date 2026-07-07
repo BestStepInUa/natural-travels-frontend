027d5a7f4f32a4f3b939e2cb6f001824bd6d386d
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2026-07-07 14:57:18 +0300

    FIXED

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,8 @@
-// // import css from './page.module.css';
-import About from "../components/About/About"
-
+import About from '@/components/About/About';
 
 export default function Home() {
   return (
     <div className="container">
-      Home page
       <About />
     </div>
   );
