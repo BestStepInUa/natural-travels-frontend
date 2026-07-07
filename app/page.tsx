cdff473b9091d0d46e1a18a17f092b7a6eb1d636
Rostik <rosticmel@gmail.com>
2026-07-07 15:34:43 +0300

    feat: add Join section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 // // import css from './page.module.css';
 
-
+import Join from '@/components/Join';
 
 export default function Home() {
   return (
     <div className="container">
       Home page
+      <Join />
     </div>
   );
 }
