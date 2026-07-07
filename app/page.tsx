0fdf44f1a726f291abd8d0ef3b27f43bc153ddd0
Liza <lizagamaj@email.com>
2026-07-07 05:20:11 +0300

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 // // import css from './page.module.css';
+import { Hero } from '@/components/Hero/Hero';
 
 
 
 export default function Home() {
   return (
     <div className="container">
-      Home page
+      <Hero/>
     </div>
   );
 }
