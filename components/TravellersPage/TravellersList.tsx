ffb9fa90f27f3c164fa6e8c636b8ffe0fb69c34b
Milena Karpenko <milenness06@gmail.com>
2026-07-08 23:25:34 +0300

    fix travellers page

--- components/TravellersPage/TravellersList.tsx
+++ components/TravellersPage/TravellersList.tsx
@@ -1,52 +1,26 @@
 'use client';
 
-import { useRef } from "react";
-import { Traveller } from "./TravellersPage";
-import { TravellerCard } from "@/components/TravellerCard/TravellerCard";
+import { Traveller } from './TravellersPage';
+import { TravellerCard } from '@/components/TravellerCard/TravellerCard';
 import css from './page.module.css';
 
 interface TravellersListProps {
   travellers: Traveller[];
-  onLoadMore: () => void;
-  hasMore: boolean;
 }
 
-export default function TravellersList({ travellers, onLoadMore, hasMore }: TravellersListProps) {
-  const listTopRef = useRef<HTMLDivElement>(null);
-
-  const handleLoadMore = () => {
-    onLoadMore();
-    if (listTopRef.current) {
-      listTopRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
+export default function TravellersList({ travellers }: TravellersListProps) {
   return (
-    <div ref={listTopRef} className={css.listContainer}>
-      <div className={css.flexContainer}>
-        {travellers.map((traveller) => (
-          <div key={traveller.id} className={css.cardWrapper}>
-            <TravellerCard
-              id={traveller.id.toString()}
+    <ul className={css.flexContainer}>
+      {travellers.map((traveller) => (
+        <li key={traveller.id} className={css.cardWrapper}>
+          <TravellerCard
+            id={traveller.id.toString()}
             name={traveller.name}
             articlesCount={traveller.articlesCount}
             photoUrl={traveller.photoUrl}
-            />
-          </div>
-        ))}
-      </div>
-
-      {hasMore && (
-        <div className={css.paginationContainer}>
-          <button
-            type='button'
-            onClick={handleLoadMore}
-            className={css.loadMoreBtn}
-          >
-            Показати ще
-          </button>
-        </div>
-      )}
-    </div>
+          />
+        </li>
+      ))}
+    </ul>
   );
-};
+}
