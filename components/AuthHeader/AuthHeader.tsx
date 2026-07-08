4e0cb7b4f3ca357cdc33a631733382397d679a86
Nikita <remnovmikita@gmail.com>
2026-07-08 11:23:21 +0300

    Прибрав помилку з іконкою та створив переадресацію на клін по іконці

--- components/AuthHeader/AuthHeader.tsx
+++ components/AuthHeader/AuthHeader.tsx
@@ -1,15 +1,16 @@
 import Image from 'next/image';
 import css from './AuthHeader.module.css';
+import Link from 'next/link';
 
 export default function AuthHeader() {
   return (
     <header className={css.header}>
       <div className="container">
-        <div className={css.content}>
-          <Image src="/icons/logo.svg" alt="icon"   width={100}   // указываем ширину
-      height={100}/>
+        <Link href="/" className={css.content}>
+          <Image src="/icons/logo.svg" alt="icon" width={24}   // указываем ширину
+      height={24}  className={css.logo}/>
           <p className={css.textAuthHeader}>Прирордні мандри</p>
-        </div>
+        </Link>
       </div>
     </header>
   );
