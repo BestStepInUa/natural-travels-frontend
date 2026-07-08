b40c38ce8307a924c2f9de299802d4fec9317747
Siela <boyko.sofania@gmail.com>
2026-07-08 08:20:44 +0200

    add StoryCard and PopularStories for HomePage

--- lib/api/storiesApi.ts
+++ lib/api/storiesApi.ts
@@ -1,17 +1,25 @@
 import { nextServer } from './api';
 
-// Отримати одну історію
 export const getStoryById = async (storyId: string) => {
   const res = await nextServer.get(`/stories/${storyId}`);
   return res.data;
 };
 
-// Зберегти історію
 export const saveStory = async (storyId: string): Promise<void> => {
   await nextServer.post(`/stories/saved/${storyId}`);
 };
 
-// Видалити зі збережених
 export const unsaveStory = async (storyId: string): Promise<void> => {
   await nextServer.delete(`/stories/saved/${storyId}`);
+};
+
+export const getPopularStories = async (page: number, perPage: number) => {
+  const res = await nextServer.get('/stories', {
+    params: {
+      type: 'popular',
+      perPage,
+      page,
+    },
+  });
+  return res.data.stories;
 };
