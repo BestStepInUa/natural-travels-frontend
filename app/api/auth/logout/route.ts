53825e2faffead473f4e1478bbdef36768bed3e2
komandor <koletttut@gmail.com>
2026-07-09 18:44:29 +0300

    add fix logout

--- app/api/auth/logout/route.ts
+++ app/api/auth/logout/route.ts
@@ -2,16 +2,29 @@ import { cookies } from "next/headers";
 import { api } from "../../api";
 import { NextResponse } from "next/server";
 
-export async function POST(){
-  const cookieStore = await cookies();
+export async function POST() {
+  try {
+    const cookieStore = await cookies();
+    await api.post(
+      'auth/logout',
+      {},
+      {
+        headers: {
+          Cookie: cookieStore.toString(),
+        },
+      }
+    );
+    cookieStore.delete("accessToken");
+    cookieStore.delete("refreshToken");
+    cookieStore.delete("sessionId");
+    return NextResponse.json({ message: 'Logged out successfully' });
+  } catch {
 
-  await api.post('auth/logout', {
-    headers:{
-      Cookies: cookieStore.toString()
-    }
-  })
-  cookieStore.delete("accessToken");
-  cookieStore.delete("refrexhToken");
+    const cookieStore = await cookies();
+    cookieStore.delete("accessToken");
+    cookieStore.delete("refreshToken");
+    cookieStore.delete("sessionId");
 
-  return NextResponse.json({message: 'Logged out successfuly'})
+    return NextResponse.json({ message: 'Logged out successfully' });
+  }
 }
