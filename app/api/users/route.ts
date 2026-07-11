ffc810da708a45e0a1a82d5d0360fc619b5f1a31
Oleksandr Vasylenko <beststep.in.ua@gmail.com>
2026-07-11 16:58:10 +0300

    FIXED api/users

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,20 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { api } from '../api';
-import { ApiError, createErrorResponce } from '../_utils/utils';
+import { NextRequest, NextResponse } from 'next/server'
+import { api } from '../api'
+import { ApiError, createErrorResponce } from '../_utils/utils'
 
 export async function GET(req: NextRequest) {
-  try {
-    const { searchParams } = new URL(req.url);
-    const page = searchParams.get('page');
-    const perPage = searchParams.get('perPage');
+	try {
+		const { searchParams } = new URL(req.url)
+		const page = searchParams.get('page')
+		const perPage = searchParams.get('perPage')
 
-    const params: Record<string, string> = {};
-    if (page) params.page = page;
-    if (perPage) params.perPage = perPage;
+		const params: Record<string, string> = {}
+		if (page) params.page = page
+		if (perPage) params.perPage = perPage
 
-    const res = await api.get('/api/users', { params });
-    return NextResponse.json(res.data);
-  } catch (error) {
-    return createErrorResponce(error as ApiError);
-  }
+		const res = await api.get('/users', { params })
+		return NextResponse.json(res.data)
+	} catch (error) {
+		return createErrorResponce(error as ApiError)
+	}
 }
