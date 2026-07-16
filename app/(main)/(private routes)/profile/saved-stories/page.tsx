76e3695a5222501bd4098026d60f4bf5bf70c41f
komandor <koletttut@gmail.com>
2026-07-16 20:37:57 +0300

    fix story and profile

--- app/(main)/(private routes)/profile/saved-stories/page.tsx
+++ app/(main)/(private routes)/profile/saved-stories/page.tsx
@@ -1,29 +1,7 @@
 'use client';
 
-import { useSavedStories } from '@/hooks/useSavedStories';
-
-import MessageNoStories from '@/components/MessageNoStories';
-import Loader from '@/components/Loader/Loader';
 import SaveStoriesList from '@/components/SaveStoriesList/SaveStoriesList';
 
 export default function SavedStoriesPage() {
-  const { data, isPending, isError } = useSavedStories(1);
-
-  if (isPending) return <Loader />;
-
-  if (isError) {
-    return <p>Не вдалося завантажити збережені історії.</p>;
-  }
-
-  if (!data || data.data.length === 0) {
-    return (
-      <MessageNoStories
-        text="У вас ще немає збережених історій."
-        buttonText="Переглянути історії"
-        linkTo="/stories"
-      />
-    );
-  }
-
   return <SaveStoriesList />;
 }
